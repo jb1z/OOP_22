f28118f6953cba8f606228355df4d1a259cb5b3a
jbiz <pozolotusiko@mail.ru>
2022-06-23 13:32:49 +0300

    copy constuctor

--- file_1.cpp
+++ file_1.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 using namespace std;
 
+class MyClassOther{
+    private:
+        int privateInfo_;
+    public:
+        int getInfo(){
+            return privateInfo_;
+        }
+};
+
 class MyClass {
     private:
-        int data_;
+        int* data_;
+        int size_;
+        int myInfo_;
     public:
-        MyClass(int data) {
-            data_ = data;
-            cout << "I'm constructor with a " << data_ << "\n";
+        MyClass(int size) {
+            size_ = size;
+            data_ = new int[size];
+            for(int i = 0; i < size; i++) {
+                data_[i] = 2 * i;
+            }
+            cout << "I'm constructor with an data adress " << data_ << "\n";
+        }
+        MyClass(MyClassOther &object) {
+            myInfo_ = object.getInfo();
+        }
+        MyClass(const MyClass &other){ // copy constuctor
+            this->data_ = new int[other.size_];
+            this->size_ = other.size_;
+            for(int i = 0; i < size_; i++){
+                data_[i] = other.data_[i];
+            }
+            cout << "I'm copy constructor\n";
         }
         ~MyClass() {
+            delete[] data_;
             cout << "I'm destructor\n";
         }
 };
@@ -19,5 +46,6 @@ int main() {
     cout << "Enter a data:\n";
     cin >> dataToEnter;   
     MyClass firstClass(dataToEnter);
+    MyClass secondClass(firstClass);
     return 0;   
 }
