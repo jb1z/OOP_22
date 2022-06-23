13ec3efb7ff7606be6bf8163dc4584faab0980ae
jbiz <pozolotusiko@mail.ru>
2022-06-23 13:06:22 +0300

    MyClass

--- file_1.cpp
+++ file_1.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{ 
-    cout << "HI HIH IH, world!" << endl;
-    int a = 0;   
-    a++;
-    int b = 0;
-    cout << b << endl;
-    cin >> b;
-    cout << b;
-    a++;
+class MyClass {
+    private:
+        int data_;
+    public:
+        MyClass(int data) {
+            data_ = data;
+            cout << "I'm constructor with a " << data_ << "\n";
+        }
+        ~MyClass() {
+            cout << "I'm destructor\n";
+        }
+};
+
+int main() {
+    int dataToEnter; 
+    cout << "Enter a data:\n";
+    cin >> dataToEnter;   
+    MyClass firstClass(dataToEnter);
     return 0;   
 }
