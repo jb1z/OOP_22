6e318923874c8454973a31d4cc225b7c65ab8d5d
jbiz <pozolotusiko@mail.ru>
2022-06-23 12:42:41 +0300

    OOP_fc

---
+++ file_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    cout << "HI HIH IH, world!" << endl;
+    int a = 0;   
+    a++;
+    int b = 0;
+    cout << b << endl;
+    cin >> b;
+    cout << b;
+    a++;
+    return 0;   
+}
---
+++ file_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
+
+int main() 
+{ 
+    cout << "HI HIH IH111111, world!" << endl;
+    int a = 0;   
+    a++; 
+    return 0; 
+}
