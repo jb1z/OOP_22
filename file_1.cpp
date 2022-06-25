9db635b32544a21c04df84fedf409c36047c603f
jbiz <pozolotusiko@mail.ru>
2022-06-25 20:44:07 +0300

    overloading == and != operator in Point and Human classes

--- file_1.cpp
+++ file_1.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Human{
+    private:
+        int age_;
+        int height_;
+        string name_;
+    public:
+        Human(int age, int height, string name){
+            age_ = age;
+            height_ = height;
+            name_ = name;
+        }
+        bool operator != (const Human &other){ // overloaded != operator
+            return !(this->age_ == other.age_ && this->height_ == other.height_ && this->name_== other.name_);
+        }
+};
+
+class Point{
+    private:
+        int x_;
+        int y_;
+    public:
+        Point(){
+            x_ = 0;
+            y_ = 0;
+        }
+        Point(int x, int y){
+            x_ = x;
+            y_ = y;
+        }
+        bool operator == (const Point &other){ // overloaded == operator
+            return this->x_ == other.x_ && this->y_ == other.y_;
+        };
+};
 
 class MyClassOther{
     private:
@@ -70,11 +105,21 @@ int main() {
     // same things
     MyClass secondObject(firstObject); // Copy constructor is used here
     MyClass thirdObject = firstObject; // And here copy constructor too, not assignment
-    //
+    // same things
     MyClassOther firstOtherObject;
     MyClass fourthObject(firstOtherObject);
     MyClass fifthObject(2);
     MyClass sixthObject(2);
     sixthObject = fifthObject = firstObject; // overloaded operator return link to object so we able to do this
+
+    Point a(1, 2);
+    Point b(1, 2);
+    bool compareResultPoint = a == b;
+    cout << compareResultPoint << "\n" ;
+
+    Human aH(19, 180, "Oleg");
+    Human bH(19, 180, "Hugo");
+    bool compareResultHuman = aH != bH;
+    cout << compareResultHuman << "\n" ;   
     return 0;   
 }
