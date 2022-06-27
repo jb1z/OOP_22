f6ac11d1b3256bc742fcb1a3850cb78cc8c0e8c1
jbiz <pozolotusiko@mail.ru>
2022-06-27 05:48:34 +0300

    static methods

--- file3.cpp
+++ file3.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 class Apple{
     private:
+        static int appleCount_;
         int weight_;
         string color_;
         int id_;
-    public:
-        static int appleCount;
+    public:       
         Apple(int weight, string color){
             weight_ = weight;
             color_ = color;
-            appleCount++;
-            setId(appleCount);
+            appleCount_++;
+            setId(appleCount_);
         }
         void setId(int id){
             id_ = id;
@@ -21,21 +21,34 @@ class Apple{
         int getId(){
             return id_;
         }
+        void print(){
+            cout << "Apple " << id_ << " weight: " << weight_ << ", color: " << color_ << "\n";
+        }
+        static int getAppleCount(){
+            // weight_ = 0; we cant access to non-static field 
+            return appleCount_;
+        }
+        static void changeColor(Apple &apple, string color){ // we can do like this
+            apple.color_ = color;  
+        }
+        
 };
 
-int Apple::appleCount = 0; // static initializing outside an object
+int Apple::appleCount_ = 0; // static initializing outside an object
 
 int main() 
 { 
-    Apple apple(150, "green");   
-    cout << "Apple id: " << apple.getId() << "\n";
+    Apple apple1(150, "green");   
+    cout << "Apple id: " << apple1.getId() << "\n";
     Apple apple2(150, "green");   
     cout << "Apple id: " << apple2.getId() << "\n";
     Apple apple3(150, "green");   
     cout << "Apple id: " << apple3.getId() << "\n";
     Apple apple4(150, "green");
     cout << "Apple id: " << apple4.getId() << "\n";
-    cout << "Amount of apples: " << Apple::appleCount << "\n";   
+    cout << "Amount of apples: " << Apple::getAppleCount() << "\n";
+    Apple::changeColor(apple2, "red");
+    apple2.print();
     return 0;
 }
 
