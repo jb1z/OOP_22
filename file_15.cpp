1e38db41addc4b010f36d5040bef6bef5e108e24
jbiz <pozolotusiko@mail.ru>
2022-06-30 19:05:24 +0300

    STL vector

--- file_15.cpp
+++ file_15.cpp
@@ -16,5 +16,28 @@ int main(){
     }
     // we can use auto instead of long type define
     // using for primitive worsens the readability of the code
+    cout << endl;
+
+    vector<int> myVector1;
+    //vector<int> myVector1(20,55); // 20 elements with data: 55
+    myVector1.reserve(100); // setting capacity
+    myVector1.push_back(10);
+    myVector1.push_back(2);
+    myVector1.push_back(56);
+    for(int i = 0; i < myVector1.size(); i++){
+        cout << myVector1[i] << endl;
+        cout << myVector1.at(i) << endl; // check for an array borders
+        // at method is slower
+    }
+    //myVector1.clear(); // delete all elements
+    myVector1.pop_back(); // delete last element
+    cout << "Size: " << myVector1.size() << endl; // vector size
+    cout << "Capacity: " << myVector1.capacity() << endl; // capacity of vector
+    myVector1.shrink_to_fit(); // fitting capacity to size
+    myVector1.empty(); // is empty
+    myVector1.resize(20);
+    for(int i = 0; i < myVector1.size(); i++){
+        cout << myVector1[i] << endl;       
+    }
     return 0;
 }
